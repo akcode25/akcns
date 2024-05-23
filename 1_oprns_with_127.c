dc26c32eeb2054d71a9434d0fbf477fe1291b38a
akcode25 <akanksha25misc>
2024-05-23 17:33:03 +0530

    1

---
+++ 1_oprns_with_127.c
@@ -0,0 +1,33 @@
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char str[]="hello world";
+	printf(str);
+	
+	char str1[11],str2[11],str3[11];
+	int i,len;
+	len=strlen(str);
+	
+	printf("\nAfter applying AND operator, corresponding ASCII values:\n");
+	for(i=0;i<len;i++){
+		str1[i]=str[i] & 127;
+		printf("%c = %d \n",str[i],str1[i]);
+	}
+
+	printf("\nAfter applying OR operator, corresponding ASCII values:\n");	
+	for(i=0;i<len;i++){
+		str2[i]=str[i] | 127;
+		printf("%c = %d \n",str[i],str2[i]);
+	}
+
+	printf("\nAfter applying XOR operator, corresponding ASCII values:\n");	
+	for(i=0;i<11;i++){
+		str3[i]=str[i] ^ 127;
+		printf("%c = %d \n",str[i],str3[i]);
+	}
+
+}
+
+
+
