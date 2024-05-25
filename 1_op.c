1092a0373e6f457f6fd7e8395fc0bebccddb929e
akcode25 <akanksha25misc>
2024-05-25 09:16:13 +0530

    my 1

--- 1_op.c
+++ 1_op.c
@@ -3,29 +3,29 @@
 
 // a-z: 97-122, A-Z: 65-90
 
-int main(){
-	char str[]="hello world";
-	printf(str);
-	
-	char str1[11],str2[11],str3[11];
-	int i,len;
-	len=strlen(str);
+int main() {
+    char str[]="hello world";
+    printf(str);
 
-	printf("\nAfter applying AND operator, corresponding ASCII values:\n");
-	for(i=0;i<len;i++){
-		str1[i]=str[i] & 127;
-		printf("%c = %d \n",str[i],str1[i]);
-	}
+    char str1[11], str2[11], str3[11];
+    int i, len;
+    len = strlen(str);
 
-	printf("\nAfter applying OR operator, corresponding ASCII values:\n");	
-	for(i=0;i<len;i++){
-		str2[i]=str[i] | 127;
-		printf("%c = %d \n",str[i],str2[i]);
-	}
+    printf ("\nASCII values after AND operation with 127:\n");
+    for (i=0; i<len; i++) {
+        str1[i] = str[i] & 127;
+        printf("%c = %d \n", str[i], str1[i]);
+    }
 
-	printf("\nAfter applying XOR operator, corresponding ASCII values:\n");	
-	for(i=0;i<11;i++){
-		str3[i]=str[i] ^ 127;
-		printf("%c = %d \n",str[i],str3[i]);
-	}
+    printf ("\nASCII values after OR operation with 127:\n");
+    for (i=0; i<len; i++) {
+        str2[i] = str[i] | 127;
+        printf("%c = %d \n", str[i], str2[i]);
+    }
+
+    printf ("\nASCII values after XOR operation with 127:\n");
+    for (i=0; i<len; i++) {
+        str3[i] = str[i] ^ 127;
+        printf("%c = %d \n", str[i], str3[i]);
+    }
 }
