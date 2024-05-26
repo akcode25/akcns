3bfef89ad92f033f6d33d48309c2c9d871b4c2a8
akcode25 <akanksha25misc>
2024-05-26 22:34:09 +0530

    des

--- 5_des.c
+++ 5_des.c
@@ -1,9 +1,6 @@
 #include<stdio.h>
-// Initial permutation (IP) table
-int IP[] = {2, 6, 3, 1, 4, 8, 5, 7};
-// Inverse initial permutation (IP^-1) table
-int IP_inverse[] = {4, 1, 3, 5, 7, 2, 8, 6};
-// S-boxes
+int IP[] = {2,6,3,1,4,8,5,7};     //initial permutation
+int IP_inv[] = {4,1,3,5,7,2,8,6}; //inverse ip
 int S0[4][4] = {
     {1, 0, 3, 2},
     {3, 2, 1, 0},
@@ -16,40 +13,40 @@ int S1[4][4] = {
     {3, 0, 1, 0},
     {2, 1, 0, 3}
 };
-// Function to perform initial permutation
-int ip(int plaintext) {
-    int result = 0;
-    for (int i = 0; i < 8; i++) {
-        result |= ((plaintext >> (8 - IP[i])) & 1) << (7 - i);
-    }
-    return result;
+
+int ip (int pt) {
+    int res = 0;
+    for (int i=0; i < 8; i++)
+        res |= ((pt >> (8 - IP[i])) & 1) << (7 - i);
+    return res;
 }
-// Function to perform inverse initial permutation
-int inverse_ip(int ciphertext) {
-    int result = 0;
-    for (int i = 0; i < 8; i++) {
-        result |= ((ciphertext >> (8 - IP_inverse[i])) & 1) << (7 - i);
-    }
-    return result;
+
+int ip_inv (int ct) {
+    int res = 0;
+    for (int i=0; i < 8; i++)
+        res |= ((ct >> (8 - IP_inv[i])) & 1) << (7 - i);
+    return res;
 }
-// Function to perform S-box substitution
-int s_box_substitution(int value, int s_box[4][4]) {
-    int row = ((value & 0b1000) >> 2) | (value & 0b0001);
-    int col = (value & 0b0110) >> 1;
+
+int s_box_sub (int val, int s_box[4][4]) {   //S-box substitution
+    int row = ((val & 0b1000) >> 2) | (val & 0b0001);
+    int col = (val & 0b0110) >> 1;
     return s_box[row][col];
 }
+
 int main() {
-    int plaintext = 0b11010110;
-    printf("Plain Text: %x\n", plaintext);
-    int ciphertext = ip(plaintext);
-    printf("Cipher Text: %x\n", ciphertext);
+    int pt = 0b11010110;
+    printf("Plaintext: %x\n", pt);
+    int ct = ip(pt);
+    printf("Ciphertext; %x\n", ct);
 
-    // Example of S-box substitution
-    int val = 0b1101; // Example value
-    int sboxres = s_box_substitution(val, S0);
-    printf("S-box result: %x\n", sboxres);
+    int val = 0b1101;
+    int sboxres = s_box_sub(val, S0);
+    printf("S-boz result: %x\n", sboxres);
 
-    int decrypted_text = inverse_ip(ciphertext);
-    printf("Decrypted Text: %x\n", decrypted_text);
+    int decrypted = ip_inv(ct);
+    printf("Decrypted text: %x\n",decrypted);
+
     return 0;
 }
+
