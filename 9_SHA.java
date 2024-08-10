42bdecd35f1c4549773da57af726e2df3fed6c04
Akanksha Kashyap <akashyapdeep@gmail.com>
2024-08-10 20:43:47 +0530

    or

--- 9_SHA.java
+++ 9_SHA.java
@@ -18,3 +18,37 @@ public class SHA {
             System.out.println("SHA-512 Hash: " + hexString.toString());  
     }
 }
+
+/*
+//or,
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+public class SHA {
+    public static String encrypt(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger no = new BigInteger(1, messageDigest);
+            String hash = no.toString(16);
+            while (hash.length() < 32) {
+                hash = "0" + hash;
+            }
+            return hash;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String args[]) throws NoSuchAlgorithmException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the string to hash: ");
+        String input = scanner.nextLine();
+        System.out.println("\nSHA-512 Hash: " + encrypt(input));
+    }
+}
+*/
+
+
