f267a811434d3c1c23078fb856e1b99b9ad503ca
Akanksha Kashyap <akashyapdeep@gmail.com>
2024-08-11 20:43:34 +0530

    Update 7_RSA.java

--- 7_RSA.java
+++ 7_RSA.java
@@ -1,19 +1,20 @@
 import java.math.BigInteger;
 import java.util.Scanner;
+
 public class RSA {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the message to be encrypted: ");
-		int msg = scanner.nextInt();
+		int msg = sc.nextInt();
 		System.out.print("Enter the first prime number (p): ");
-		int p = scanner.nextInt();
+		int p = sc.nextInt();
 		System.out.print("Enter the second prime number (q): ");
-		int q = scanner.nextInt();
+		int q = sc.nextInt();
 
 		int n = p * q;
 		int z = (p - 1) * (q - 1);     //ϕ(n)
 		
-		int d = 0;	//private key
+		int d = 0;  //private key
 		for (d = 2; d < z; d++) 
 			if (gcd(d, z) == 1) break;    //=> d, z - relatively prime
 
@@ -26,18 +27,14 @@ public class RSA {
 			}
 		}
 
-		// System.out.println("\nz value = " + z);
-		// System.out.println("d value = " + d);
-		// System.out.println("e value = " + e);
-
 		BigInteger bigMsg = BigInteger.valueOf(msg);
 		BigInteger bigN = BigInteger.valueOf(n);
-		BigInteger encryptedMsg = bigMsg.modPow(BigInteger.valueOf(e), bigN);  //ciphertext = (message^e) mod n
-		BigInteger decryptedMsg = encryptedMsg.modPow(BigInteger.valueOf(d), bigN);  //decryptedMessage = (ciphertext^d) mod n
+		BigInteger encryptedMsg = bigMsg.modPow(BigInteger.valueOf(e), bigN);  	     //C = (M^e) mod n
+		BigInteger decryptedMsg = encryptedMsg.modPow(BigInteger.valueOf(d), bigN);  //M = (C^d) mod n
 
 		System.out.println("\nEncrypted message is : " + encryptedMsg);
 		System.out.println("Decrypted message is : " + decryptedMsg);  //.intValue()
-		scanner.close();
+		sc.close();
 	}
 
 	static int gcd(int a, int b) {
@@ -47,6 +44,9 @@ public class RSA {
 }
 //(any number < 9) , 3, 5
 
+// 2,3,5 //2,3,11
+
+
 
 /*
 import java.math.*;
@@ -55,16 +55,12 @@ import java.util.*;
 class RSA {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Enter the message to be encrypted:");
         int msg = scanner.nextInt();
-
         System.out.println("Enter the first prime number (p):");
         int p = scanner.nextInt();
-
         System.out.println("Enter the second prime number (q):");
         int q = scanner.nextInt();
-
         int n = p * q;
         int z = (p - 1) * (q - 1);
         System.out.println("the value of z = " + z);
@@ -89,7 +85,6 @@ class RSA {
 
         double c = (Math.pow(msg, e)) % n;
         System.out.println("Encrypted message is : " + c);
-
         BigInteger N = BigInteger.valueOf(n);
         BigInteger C = BigDecimal.valueOf(c).toBigInteger();
         BigInteger msgback = (C.pow(d)).mod(N);
@@ -97,7 +92,6 @@ class RSA {
 
         scanner.close();
     }
-
     static int gcd(int e, int z) {
         if (e == 0)
             return z;
