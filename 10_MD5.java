ff9c34623729c33d9d6c14b5ff235fae7f0de4e2
Akanksha Kashyap <akashyapdeep@gmail.com>
2024-08-11 23:19:38 +0530

    Update 10_MD5.java

--- 10_MD5.java
+++ 10_MD5.java
@@ -1,19 +1,22 @@
 import java.security.MessageDigest;
 import java.util.Scanner;
+
 public class MD5{
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the text to hash: ");
+        System.out.print("Text to hash: ");
         String text = sc.nextLine();
-            MessageDigest md = MessageDigest.getInstance("MD5");           
-            byte[] messageDigest = md.digest(text.getBytes());
-            StringBuilder hexString = new StringBuilder();
-            for (byte b : messageDigest) {
-                String hex = Integer.toHexString(0xff & b);
-                if (hex.length() == 1) hexString.append('0');
-                hexString.append(hex);
-            }
-            System.out.println("MD5 Hash: " + hexString.toString());
+
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] mdBytes = md.digest(text.getBytes());
+        StringBuilder hexStr = new StringBuilder();
+
+        for (byte b: mdBytes) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) hexStr.append('0');
+            hexStr.append(hex);
+        }
+        System.out.println("MD5 hash: " + hexStr.toString());
     }
 }
 
