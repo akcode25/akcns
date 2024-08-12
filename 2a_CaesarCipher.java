5f0ff963d8a51287c2e267ae81b8636d4db2f180
Akanksha Kashyap <akashyapdeep@gmail.com>
2024-08-12 19:41:13 +0530

    more understandable

--- 2a_CaesarCipher.java
+++ 2a_CaesarCipher.java
@@ -1,23 +1,15 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.util.Scanner;
+
+import java.io.*;
 import java.util.Scanner;
 
 public class CaesarCipher {
-    static Scanner sc = new Scanner(System.in);
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static void main(String[] args) throws IOException {
-        System.out.println("Enter any String:");
-        String str = br.readLine();
-        System.out.println("Enter the key:");
-        int key = sc.nextInt();
-        
-        String encryptedString = processString(str, key, true);
-        System.out.println("Encrypted string is: " + encryptedString);
-        
-        String decryptedString = processString(encryptedString, key, false);
-        System.out.println("Decrypted string is: " + decryptedString);
-    }
+    static Scanner sc = new Scanner(System.in);    //integer input (key)
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //string input (plaintext) 
+    
     public static String processString(String str, int key, boolean encrypt) {
         StringBuilder result = new StringBuilder();
         int direction = encrypt ? 1 : -1;
@@ -26,10 +18,38 @@ public class CaesarCipher {
             if (Character.isUpperCase(ch)) 
                 ch = (char) ('A' + (ch - 'A' + direction * key + 26) % 26);
             else if (Character.isLowerCase(ch))
-                ch = (char) ('a' + (ch - 'a' + direction * key + 26) % 26);
+                ch = (char) ('a' + (ch-'a' + direction*key + 26) % 26);
             result.append(ch);
         }
         return result.toString();
     }
+
+    public static void main(String[] args) throws IOException {
+        System.out.print("\nEnter any String: ");
+        String str = br.readLine();
+        System.out.print("Enter the key: ");
+        int key = sc.nextInt();
+        
+        String en = processString(str, key, true);
+        System.out.print("Encrypted string: " + en);        
+        String de = processString(en, key, false);
+        System.out.print("\nDecrypted string: " + de);
+    }
 }
 
+/*
+Step 1: Convert the Character to Its Alphabet Position
+            ch - 'A'
+
+Step 2: Apply the Shift (Key) with Direction     
+            (ch-'A') + direction * key +26        //so that +ve
+
+Step 3: 0 to 25 only. (not >= 26)
+            ((ch-'A') + direction*key +26) % 26
+
+Step 4: ASCII value back
+     ('A' + ((ch-'A') + direction*key +26) % 26)
+
+Step 5: Convert Back to a Character
+(char)('A' + ((ch-'A') + direction*key +26) % 26) 
+*/
