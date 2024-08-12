c40e2a28577bc78497499c2c88368c2cccffe831
Akanksha Kashyap <akashyapdeep@gmail.com>
2024-08-12 20:02:19 +0530

    more understandable

--- 2a_CaesarCipher.java
+++ 2a_CaesarCipher.java
@@ -7,8 +7,8 @@ import java.io.*;
 import java.util.Scanner;
 
 public class CaesarCipher {
-    //static Scanner sc = new Scanner(System.in);    //integer input (key)
-    //static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //string input (plaintext) 
+    // static Scanner sc = new Scanner(System.in);    //integer input (key)
+    // static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //string input (plaintext) 
     
     public static String processString(String str, int key, boolean encrypt) {
         StringBuilder result = new StringBuilder();
@@ -16,9 +16,9 @@ public class CaesarCipher {
 
         for (char ch : str.toCharArray()) {
             if (Character.isUpperCase(ch)) 
-                ch = (char) ('A' + (ch - 'A' + direction * key + 26) % 26);
+                ch = (char) ((ch - 'A' + direction * key + 26) % 26 + 'A');
             else if (Character.isLowerCase(ch))
-                ch = (char) ('a' + (ch-'a' + direction*key + 26) % 26);
+                ch = (char) ((ch-'a' + direction*key + 26) % 26 + 'a');
             result.append(ch);
         }
         return result.toString();
@@ -26,9 +26,9 @@ public class CaesarCipher {
 
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        
         System.out.print("\nEnter any String: ");
-        String str = sc.nextLine();          // String str = br.readLine();        
+        // String str = br.readLine();
+        String str = sc.nextLine();        
         System.out.print("Enter the key: ");
         int key = sc.nextInt();
         
@@ -38,7 +38,6 @@ public class CaesarCipher {
         System.out.print("\nDecrypted string: " + de);
 
         sc.close();
-
     }
 }
 
@@ -47,14 +46,15 @@ Step 1: Convert the Character to Its Alphabet Position
             ch - 'A'
 
 Step 2: Apply the Shift (Key) with Direction     
-            (ch-'A') + direction * key +26        //so that +ve
+            ch-'A' + direction * key +26        //so that +ve
 
 Step 3: 0 to 25 only. (not >= 26)
-            ((ch-'A') + direction*key +26) % 26
+            (ch-'A' + direction*key +26) % 26
 
 Step 4: ASCII value back
-     ('A' + ((ch-'A') + direction*key +26) % 26)
+            ((ch-'A') + direction*key +26) % 26 + 'A')
 
 Step 5: Convert Back to a Character
-(char)('A' + ((ch-'A') + direction*key +26) % 26) 
+     (char) ((ch-'A' + direction*key +26) % 26 + 'A');
 */
+
